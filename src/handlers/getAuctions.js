eaa9f811e2d6447926d3d258a1c427256faa2d92
Harshit Raj <harshit.raj@itential.com>
2023-08-27 04:20:18 +0530

    createAuctions and getAuctions added

---
+++ src/handlers/getAuctions.js
@@ -0,0 +1,21 @@
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  const results = await dynamodb
+    .scan({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+    })
+    .promise();
+
+  auctions = results.Items;
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = getAuctions;
